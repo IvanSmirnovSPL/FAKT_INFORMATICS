65ff0ebae06eef2c65c4a3db3c99983cc1090eb9
Ivan Smirnov <smirnov.ie@phystech.edu>
2025-11-10 18:26:23 +0300

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -2,25 +2,52 @@
 
 using namespace std;
 
+void show(double*  arr, unsigned int size)
+{
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void setMinusOnes(double*  arr, unsigned int size)
+{
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        arr[i] = -1;
+    }
+}
+
+void expand(
+    double** pointer,
+    unsigned int curSize,
+    unsigned int desiredSize
+)
+{
+    double* curArray = *pointer;
+    double* tmp = new double[desiredSize];
+    setMinusOnes(tmp, desiredSize);
+    for (unsigned int i = 0; i < curSize; ++i)
+    {
+        tmp[i] = curArray[i];
+    }
+    delete [] *pointer;
+    *pointer = tmp;
+}
+
 int main()
 {
-    int width;
-    int height;
-    cin >> width >> height;
+    double* arr = new double[3];
+    setMinusOnes(arr, 3);
+    arr[0] = 1;
+    arr[1] = 1000;
+    show(arr, 3);
+    expand(&arr, 3, 10);
+    show(arr, 10);
 
-    float** mat = new float*[height];
-    for(int i = 0; i < height; ++i)
-    {
-        mat[i] = new float[width];
-    } 
 
-    mat[height - 1][width - 1] = 100;
-    cout << mat[height - 1][width - 1] << endl;
 
-    for(int i = 0; i < height; ++i)
-    {
-        delete [] mat[i];
-    } 
-    delete mat;
+    delete [] arr;
 
 }
