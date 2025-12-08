307958b96b78459072ff755d63f311d5a7a0773c
Ivan Smirnov <smirnov.ie@phystech.edu>
2025-12-08 18:19:42 +0300

    upd

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+bool push_forward(subforwardlist** sfl, int d) {
+    if (sfl == nullptr) {
+        return false;
+    }
+    if (*sfl == NULL) {
+        *sfl = new subforwardlist;
+        (*sfl)->data = d;
+        (*sfl)->next = *sfl;
+        return true;
+    }
+    subforwardlist* new_h = new subforwardlist;
+    new_h->data = d;
+    new_h->next = *sfl;
+    sfl = &new_h;
+    return true;
 }
+
+subforwardlist* a = new subforwardlist;
+push_forward(&a, 5);
